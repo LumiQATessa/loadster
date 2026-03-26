389ae9a664aa70421641288517faf49d1b75e59b
Tessa Atkinson-van Veen <tessa.vanveen@lumiglobal.com>
2026-03-26 17:02:29 +0000

    more credentialed participant uopdates

---
+++ Helpers/TandC.js
@@ -0,0 +1,3 @@
+browser.wait();
+browser.click('#checkbox', { timeout: 40000, silent: true });
+browser.click('btn-accept>*', { timeout: 40000, silent: true });
---
+++ Tests/CPLogin.js
@@ -0,0 +1,10 @@
+//wait for the page to load
+
+//wait for #username to be visible
+
+//enter username and password 
+
+//click
+browser.click('button[value="login"]', { timeout: 40000, silent: true });
+
+//wait for home-page-panel
